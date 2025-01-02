a4801070d39ad725fb5442d8a6ea5853cce490b6
Justin Findlay <jfindlay@cloudflare.com>
2025-01-02 18:18:53 -0500

    Various improvements
    - Move tox configs into `pyproject.toml`
    - Fix `mpypy` and `ruff` checking problem where the `.pyi` file was
      getting in the way and fix subsequent typing errors
    - Remove `async` form of `exec_cmd`.  It has been problematic and
      `logging` does not even support async.  Someone else may have solved
      async logging in a way that integrates with `logging`, but a more
      serious problem is that `subprocess` cannot stream from both `stdout`
      and `stderr` simultaneously, as concurrent threads.  Both of these are
      much bigger problems and streaming from `stdout` or `stdout+stderr` is
      convenient and a oneline with this module, the general case (async
      stderr+stdout+logging) is unimplemented by the python stdlibs.  Until
      I or someone else addresses each of those, this module is a cute
      convenience with trivial value.  If/when those features are
      implemented this module may even be able to be deprecated.  It's
      possible the `asyncio.create_subprocess_exec` does this but unless the
      logger can accommodate async inputs, it's not going to work.  I should
      explore this again later.
    - Minor README edits and add section on package update process
    - Update license field to standard [SPDX expression](https://packaging.python.org/en/latest/guides/writing-pyproject-toml/#license)

--- README.md
+++ README.md
@@ -4,21 +4,37 @@ Collection of simple utility functions and classes that extend standard library 
 
 ## Installation
 
-```
-pip install xjfx
+```bash
+$ pip install xjfx
 ```
 
 ## Development
 
 Check for code lint errors:
-```
-tox run-parallel -m analyze
+```bash
+$ tox run-parallel -m analyze
 ```
 Enforce code formatting:
-```
-tox run-parallel -m edit
+```bash
+$ tox run-parallel -m edit
 ```
 Both:
+```bash
+$ tox run-parallel -m edit analyze
 ```
-tox run-parallel -m edit analyze
-```
+
+### Create a release
+
+1. `tox p -m analyze edit`
+2. `python3 -m build --wheel`
+3. `twine check dist/*`
+4. Update `version` field in `pyproject.toml` to `<version>` and `git tag -a <version>`
+5. Push `master` and `<version>` tag to github
+6. `twine upload --repository testpypi dist/*`
+7.
+  ```bash
+  $ virtualenv venv
+  $ venv/bin/pip install --index-url https://test.pypi.org/simple/ xjfx
+  $ venv/bin/python -c 'import xjfx'
+  ```
+8. `twine upload --repository pypi dist/*`
--- src/xjfx.py
+++ src/xjfx.py
@@ -4,12 +4,12 @@ Collection of simple utility functions and classes that extend standard library 
 For convenience, the `DEVNULL`, `STDOUT`, and `PIPE` constants are imported from `subprocess` so that users of `xjfx` do not
 need to `import subprocess`.
 """
+
 # TODO:
 # - Tests
 # - exec_cmd
 # 	- Multiproc support (including logging) for watching proc stdout and stderr in real time
 
-import asyncio
 import collections
 import concurrent.futures
 import enum
@@ -19,16 +19,17 @@ import logging
 import shlex
 import subprocess
 import typing
+from dataclasses import dataclass
 
 import colorama
 
 DEVNULL = subprocess.DEVNULL
 STDOUT = subprocess.STDOUT
 PIPE = subprocess.PIPE
-LOGGER = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
-class ProcStreamClassifier(enum.Enum):
+class ProcStreamClassifier(enum.IntEnum):
     """
     Enumerate process "streams" used by the `exec_cmd*` functions.
     - OUTPUT: Combined stdout/stderr
@@ -36,77 +37,46 @@ class ProcStreamClassifier(enum.Enum):
     - STDERR: Standard error
     """
 
-    OUTPUT = 0
-    STDOUT = 1
-    STDERR = 2
+    OUTPUT = enum.auto()
+    STDOUT = enum.auto()
+    STDERR = enum.auto()
 
 
+@dataclass
 class ProcData:
     """
     Data returned from a command.
     """
 
-    def __init__(self, decode_output: bool = True):
-        """
-        Setup command return object.
-        """
-        self.decode_output = decode_output
-        self._stdout = b""
-        self._stderr = b""
-        self.retcode = 0
-
-    @property
-    def stdout(self) -> str | bytes:
-        """
-        Transparently decode if necessary.
-        """
-        return self._stdout.decode() if self.decode_output else self._stdout
-
-    @stdout.setter
-    def stdout(self, lines:list[bytes]) ->None:
-        """
-        Append to stdout.
-        """
-        self._stdout = lines
-
-    @property
-    def stderr(self) -> str | bytes:
-        """
-        Transparently decode if necessary.
-        """
-        return self._stderr.decode() if self.decode_output else self._stderr
-
-    @stderr.setter
-    def stderr(self, lines:list[bytes]) ->None:
-        """
-        Append to stderr.
-        """
-        self._stderr = lines
+    stdout: bytes | str
+    stderr: bytes | str
+    retcode: int
 
 
 def _fmt_proc_cmd(args: list[str]) -> list[str]:
     """
     Format command preamble.
     """
-    return (
+    return [
         "%s[executing]%s `%s`",
         colorama.Fore.WHITE + colorama.Style.BRIGHT,
         colorama.Style.RESET_ALL,
         shlex.join(args),
-    )
+    ]
 
 
 def _fmt_proc_output(stream_class: ProcStreamClassifier, line: str) -> list[str]:
     """
     Format stdout/stderr logging output.
     """
+    fore_color = ""
     if stream_class == ProcStreamClassifier.OUTPUT:
         fore_color = colorama.Fore.LIGHTBLACK_EX
     if stream_class == ProcStreamClassifier.STDOUT:
         fore_color = colorama.Fore.BLUE
     elif stream_class == ProcStreamClassifier.STDERR:
         fore_color = colorama.Fore.YELLOW
-    return (
+    return [
         # Sometimes the cmd output does not return to the line beginning, so force carriage return
         "    %s[%s]%s %s%s%s\r",
         fore_color + colorama.Style.BRIGHT,
@@ -115,43 +85,32 @@ def _fmt_proc_output(stream_class: ProcStreamClassifier, line: str) -> list[str]
         fore_color,
         line.strip(),
         colorama.Style.RESET_ALL,
-    )
+    ]
 
 
-def _iterate_proc_output(stream: io.BufferedReader, stream_class: ProcStreamClassifier) -> list[bytes]:
+def _iterate_proc_output(stream: io.BytesIO | None, stream_class: ProcStreamClassifier) -> bytes:
     """
     Iterate over the process's stdout/stderr.
     """
-    accumulated_output = b''
-    for raw_line in stream:
-        LOGGER.debug(*_fmt_proc_output(stream_class, raw_line.decode()))
+    accumulated_output = b""
+    for raw_line in stream or []:
+        logger.debug(*_fmt_proc_output(stream_class, raw_line.decode()))
         accumulated_output += raw_line
     return accumulated_output
 
 
-async def _iterate_proc_output_async(stream: io.BufferedReader, stream_class: ProcStreamClassifier) -> list[bytes]:
+def _display_proc_result(args: list[str], ignore_retcode: bool, proc_data: ProcData) -> None:
     """
-    Iterate over the process's stdout/stderr.
-    """
-    accumulated_output: b''
-    async for raw_line in stream:
-        LOGGER.debug(*_fmt_proc_output(stream_class, raw_line.decode()))
-        accumulated_output += raw_line
-    return accumulated_output
-
-
-def _display_proc_result(args:list[str], ignore_retcode:bool, proc_data:ProcData)->None:
-    '''
     Show command data in the case of error.
-    '''
+    """
     if not ignore_retcode and proc_data.retcode != 0:
-        LOGGER.error(f"`{shlex.join(args)}` returned: {proc_data.retcode!r}")
+        logger.error(f"`{shlex.join(args)}` returned: {proc_data.retcode!r}")
         # If the log level is debug or lower, this info was already logged.
-        if LOGGER.getEffectiveLevel() > logging.DEBUG:
+        if logger.getEffectiveLevel() > logging.DEBUG:
             if proc_data.stdout:
-                LOGGER.error(proc_data.stdout)
+                logger.error(proc_data.stdout)
             if proc_data.stderr:
-                LOGGER.error(proc_data.stderr)
+                logger.error(proc_data.stderr)
 
 
 def exec_cmd(
@@ -161,7 +120,6 @@ def exec_cmd(
     stderr: int | None = PIPE,
     cwd: str | None = None,
     ignore_retcode: bool = False,
-    decode_output: bool = True,
     **kwargs,
 ) -> ProcData:
     """
@@ -171,15 +129,17 @@ def exec_cmd(
     - Integrate logging
     - Format the results
 
-    Except for `ignore_retcode`, and `decode_output`, the arguments are identical to `subprocess.Popen()` and are passed
-    directly to that constructor.
+    Except for `ignore_retcode`, the arguments are identical to `subprocess.Popen()` and are passed directly to that
+    constructor.
 
     The `subprocess` module cannot natively watch both stdout and stderr in real time unless stderr is redirected to stdout.
     Thus, this function can watch stdout or stderr but not both unless they are combined.  Combining stdout and stderr streams
     by setting `stderr=subprocess.STDOUT` means they will be indistinguishably returned in `proc_data.stdout`.
     """
-    LOGGER.debug(*_fmt_proc_cmd(args))
-    proc_data = ProcData(decode_output=decode_output)
+    logger.debug(*_fmt_proc_cmd(args))
+
+    stdout_data: bytes | str
+    stderr_data: bytes | str
 
     with subprocess.Popen(
         args,
@@ -189,58 +149,20 @@ def exec_cmd(
         cwd=cwd,
         **kwargs,
     ) as proc_desc:
-        if input is not None:
+        if input is not None and proc_desc.stdin is not None:
             proc_desc.stdin.write(input)
             proc_desc.stdin.flush()
         if stdout:
-            proc_data.stdout = _iterate_proc_output(
-                proc_desc.stdout, ProcStreamClassifier.OUTPUT if stderr == STDOUT else ProcStreamClassifier.STDOUT
+            stdout_data = _iterate_proc_output(
+                proc_desc.stdout,  # type: ignore[arg-type]
+                (ProcStreamClassifier.OUTPUT if stderr == STDOUT else ProcStreamClassifier.STDOUT),
             )
         if stderr and stderr != STDOUT:
-            proc_data.stderr = _iterate_proc_output(proc_desc.stderr, ProcStreamClassifier.STDERR)
-    proc_data.retcode = proc_desc.returncode
-
-    _display_proc_result(args, ignore_retcode, proc_data)
-    return proc_data
-
-
-async def exec_cmd_async(
-    args: list[str],
-    input: bytes | None = None,
-    stdout: int | None = PIPE,
-    stderr: int | None = PIPE,
-    cwd: str | None = None,
-    ignore_retcode: bool = False,
-    decode_output: bool = True,
-    **kwargs,
-) -> ProcData:
-    """
-    Similar to `exec_cmd()` but using `asyncio` subprocess primitives.
-
-    Except for `ignore_retcode`, and `decode_output`, the arguments are identical to `asyncio.create_subprocess_exec()` and are
-    passed directly to that function, which pass the remainder to `subprocess.Popen()`.
-    """
-    LOGGER.debug(*_fmt_proc_cmd(args))
-    proc_data = ProcData(decode_output=decode_output)
-    proc_desc = asyncio.create_subprocess_exec(
-        args[0],
-        *args[1:],
-        stdin=None if input is None else PIPE,
-        stdout=stdout,
-        stderr=stderr,
-        cwd=cwd,
-        **kwargs,
-    )
-    if input is not None:
-        proc_desc.stdin.write(input)
-        proc_desc.stdin.flush()
-    if stdout:
-        proc_data.stdout = _iterate_proc_output_async(
-            proc_desc.stdout, ProcStreamClassifier.OUTPUT if stderr == STDOUT else ProcStreamClassifier.STDOUT
-        )
-    if stderr and stderr != STDOUT:
-        proc_data.stderr = _iterate_proc_output_async(proc_desc.stderr, ProcStreamClassifier.STDERR)
-    proc_data.retcode = proc_desc.returncode
+            stderr_data = _iterate_proc_output(
+                proc_desc.stderr,  # type: ignore[arg-type]
+                ProcStreamClassifier.STDERR,
+            )
+    proc_data = ProcData(stdout_data, stderr_data, proc_desc.returncode)
 
     _display_proc_result(args, ignore_retcode, proc_data)
     return proc_data
@@ -322,10 +244,10 @@ class GrouperIncomplete(enum.Enum):
     remainder: Keep partial last block
     """
 
-    FILL = 1
-    STRICT = 2
-    IGNORE = 3
-    REMAINDER = 4
+    FILL = enum.auto()
+    STRICT = enum.auto()
+    IGNORE = enum.auto()
+    REMAINDER = enum.auto()
 
 
 def grouper(
@@ -356,7 +278,7 @@ def grouper(
         # Can u read it?  One more, unitary iterator for the remainder.
         remainder = iter([tuple(i[-(len(i) % n) :])]) if len(i) % n != 0 else iter(())
         return itertools.chain(zip(*args), remainder)
-    raise ValueError("Expected fill, strict, ignore, or remainder")
+    raise ValueError("Expected one of {', '.join(GrouperIncomplete)}")
 
 
 def thr_exec(func: collections.abc.Callable, args: list[tuple], max_workers: int | None = None):
@@ -368,4 +290,4 @@ def thr_exec(func: collections.abc.Callable, args: list[tuple], max_workers: int
             try:
                 future.result()
             except Exception as ex:
-                LOGGER.error(f"Error executing task: {ex}")
+                logger.error(f"Error executing task: {ex}")
